18e8f0ca3b43c72b17a8054d07923cfed5e3031e
Andreas Nuyts <andreas.nuyts@kuleuven.be>
2025-08-14 17:25:40 +0200

    Newline in author list.

--- conf.py
+++ conf.py
@@ -172,7 +172,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'logic_and_proof.tex', u'Logic and Proof',
-     u'Jeremy Avigad, Joseph Hua, Robert Y. Lewis, and Floris van Doorn', 'manual'),
+     u'Jeremy Avigad, Joseph Hua, Robert Y. Lewis,\\\\and Floris van Doorn', 'manual'),
 ]
 
 
